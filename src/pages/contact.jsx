be6b17afa38e9f24fdd2afd3503872a53e4cb73d
saylessma <beshaneh@gmail.com>
2024-08-10 20:47:08 +0300

    contact input color change

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -13,25 +13,25 @@ function Contact(){
             <div className="form">
             <label style={{margin:20}} >Your Name</label>
             <input style={{margin:20,height:30, width:200, borderRadius:5,
-                 border:'none', backgroundColor:'#fff', color:'inherit', overflowX:'hidden'}} type="text" 
+                 border:'none', backgroundColor:'#fff', color:'black', overflowX:'hidden'}} type="text" 
                  placeholder="Name" /><br />
 
             <label style={{margin:20}} >Email Address</label>
             <input style={{margin:20, height:30, width:200, borderRadius:5,
-                 border:'none',backgroundColor:'#fff', color:'inherit', overflowX:'hidden'}} type="email" 
+                 border:'none',backgroundColor:'#fff', color:'black', overflowX:'hidden'}} type="email" 
                  placeholder="Email Address"/>
                  <br />
 
 
             <label style={{margin:20}} >Phone Number</label>
             <input style={{margin:20, height:30, width:200, borderRadius:5, 
-                border:'none',backgroundColor:'#fff', color:'inherit', overflowX:'hidden'}} type="tel" 
+                border:'none',backgroundColor:'#fff', color:'black', overflowX:'hidden'}} type="tel" 
                 placeholder="Phone Number" />
 
 
             </div>
             <label style={{margin:20}}>Message</label>
-            <textarea style={{margin:20, width:'90%', color:"inherit" ,backgroundColor:'#fff', overflowX:'hidden'}} type='text'  placeholder="message" rows={10} cols={100}></textarea>
+            <textarea style={{margin:20, width:'90%', color:'black' ,backgroundColor:'#fff', overflowX:'hidden'}} type='text'  placeholder="message" rows={10} cols={100}></textarea>
         </form>
         <h1 style={{color:'#53b7ff', fontSize:'12px', fontWeight:200,marginLeft:'43.5%'}}>{submit}</h1>
 
